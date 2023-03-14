0db0638e8f77deeb892d837cd01810d9d8ce7286
Martin Labryga <martin@labryga.com>
2023-03-14 10:14:34 +0100

    nodejs user updated

--- webpack/build_image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker image build -t node_user:lts-alpine3.17 \
-  --build-arg host_user_name=$USER \
-  --build-arg host_user_id=$(id -u) \
-  -f ./node_user.Dockerfile .
-
-docker rmi -f $(docker images -f dangling=true -q)
